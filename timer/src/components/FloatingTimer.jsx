7ba66d52343119b83dc36efacca9b75b39633a3a
KRUPAL MEWADA <krupalmewada17@gmail.com>
2026-06-27 00:18:24 -0400

    feat: wire useDrag into FloatingTimer with fixed positioning

--- timer/src/components/FloatingTimer.jsx
+++ timer/src/components/FloatingTimer.jsx
@@ -2,21 +2,34 @@ import useTimer from "../hooks/useTimer";
 import TimerDisplay from "./TimerDisplay";
 import TimerControls from "./TimerControls";
 import SessionCount from "./SessionCount";
+import useDrag from "../hooks/useDrag";
 export default function FloatingTimer() {
   const { timeLeft, isRunning, isBreak, sessions, start, pause, reset } =
     useTimer();
+  const { position, handleMouseDown } = useDrag();
   return (
-    <>
+    <div
+      style={{
+        position: "fixed",
+        left: position.x,
+        top: position.y,
+        zIndex: 2147483647,
+        cursor: "grab",
+        background: "#1a1a1a",
+        padding: "12px",
+        borderRadius: "12px",
+        width: "200px",
+      }}
+      onMouseDown={handleMouseDown}
+    >
       <TimerDisplay timeLeft={timeLeft} isBreak={isBreak} sessions={sessions} />
-
       <TimerControls
         isRunning={isRunning}
         start={start}
         pause={pause}
         reset={reset}
       />
-
       <SessionCount sessions={sessions} />
-    </>
+    </div>
   );
 }
